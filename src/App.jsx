e1c005b32a4bd69da3d2f2ef61db023ce0c57fcc
lovanshgaur <lovanshgaur@gmail.com>
2026-03-26 16:32:18 +0530

    feat - input submission working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [message, setMessage] = useState("");
 
- return(
-  <>
-  v2 init - GitPeek_v2 - Lovansh Gaur
-  </>
- )
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Submitted:", text);
+    setMessage(`Input taken - ${text}`);
+    setText("");
+
+    setTimeout(() => {
+      setMessage("");
+    }, 2000);
+  };
+
+  return (
+    <div>
+      {message ? <div className="message">{message}</div> : ""}
+      <form onSubmit={handleSubmit}>
+        <label>Enter Your Username</label>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
